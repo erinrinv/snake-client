8ed28cb9890a1f38b1e5c3e2ae3aa60074094310
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-14 09:25:18 -0700

    updated input.js with input for up down left right and referenced it

--- input.js
+++ input.js
@@ -1,9 +1,13 @@
 // setup interface to handle user input from stdin
 
+let connection;
+
 const setupInput = function () {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
+  stdin.on('data', handleUserInput);
   stdin.resume();
   return stdin;
 };
@@ -15,6 +19,21 @@ const handleUserInput = function() {
       process.exit();
     }
   });
+
+  if(key === MOVE_UP_KEY){
+    connnection.write('Move: up');
+  }
+  if(key === MOVE_LEFT_KEY){
+    connnection.write('Move: left');
+  }
+  if(key === MOVE_DOWN_KEY){
+    connnection.write('Move: down');
+  }
+  if(key === MOVE_RIGHT_KEY){
+    connnection.write('Move: right');
+  }
+
+
 };
 
 
